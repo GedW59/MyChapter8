a947e7594aa7ca7cf57116847f8c1b2637fe5927
Gerard White <gwhite1965@outlook.com>
2025-07-15 16:17:49 +0100

    First commit: Integers and strings.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using System;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Working with big integers.
+var largest = ulong.MaxValue;
+Console.WriteLine($"A large number {largest,40:N0}");
+
+var atomsIntheUniverse = BigInteger.Parse("123456789012345678901234567890");
+Console.WriteLine($"Estimated atoms in the universe {atomsIntheUniverse,40:N0}");
+
+
+//Working with Complex numbers.
+var c1 = new Complex(4, 2);
+var c2 = new Complex(3, 7);
+var c3 = c1 + c2;
+Console.WriteLine($"{c1} added to {c2} is {c3}");
+
---
+++ WorkingWithText/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+string city = "London";
+Console.WriteLine($"{city} is {city.Length} characters long.");
+
+Console.WriteLine($"First char {city[0]} and the third char is {city[2]}");
+
+
+string cities = "Paris,Berlin,Madrid,New York";
+string[] citiesArray = cities.Split(",");
+foreach (string item in citiesArray)
+{
+    Console.WriteLine(item);
+}
+
+
+string fullName = "Alan Jones";
+int indexOfTheSpace = fullName.IndexOf(" ");
+
+string firstName = fullName.Substring(startIndex: 0, length: indexOfTheSpace);
+
+string lastName = fullName.Substring(startIndex: indexOfTheSpace + 1);
+
+Console.WriteLine($" {firstName}  {lastName}");
+
+string company = "Microsoft";
+bool startsWithM = company.StartsWith("M");
+bool containsN = company.Contains("N");
+
+Console.WriteLine($"Starts with M: {startsWithM}, contains N: {containsN}");
+
+string recombined = string.Join(" => ", citiesArray);
+
+Console.WriteLine(recombined);
+
+string fruit = "Apples";
+decimal price = 0.39M;
+DateTime when = DateTime.Today;
+
+Console.WriteLine($"{fruit} cost {price:C} on {when:dddd}s");
+Console.WriteLine(string.Format("{0} cost {1:C} on {2:dddd}s",fruit, price, when));
+
+
