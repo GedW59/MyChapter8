bc4f668c86e39f963b9b8347725a4941fc7137c6
Gerard White <gwhite1965@outlook.com>
2025-07-18 11:38:42 +0100

    Working with distionaries first commit

---
+++ WorkingWithDictionaries/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+var keywords = new Dictionary<string, string>();
+
+
+keywords.Add("int", "32-bit integer data type");
+keywords.Add("long", "64-bit integer data type");
+keywords.Add("float", "Single precision floating point number");
+
+Console.WriteLine("Keywords and their definitions");
+
+foreach (KeyValuePair<string, string> item in keywords)
+{
+    Console.WriteLine($"{item.Key} : {item.Value}");
+}
+
+Console.WriteLine($"The definition of long is {keywords["long"]}");
+
