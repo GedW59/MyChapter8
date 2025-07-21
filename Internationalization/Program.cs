ffb896cdd79977c19a944b1cd656c0830dd46458
Gerard White <gwhite1965@outlook.com>
2025-07-21 14:06:59 +0100

    internationalization commit

---
+++ Internationalization/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.Globalization;
+using static System.Console;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CultureInfo globalization = CultureInfo.CurrentCulture;
+        CultureInfo localization = CultureInfo.CurrentUICulture;
+
+        WriteLine("The current globalization culture is {0}: {1}", globalization.Name, globalization.DisplayName);
+        WriteLine("The current localization culture is {0}: {1}", localization.Name, localization.DisplayName);
+        WriteLine();
+
+        WriteLine("en-US: English (United States)");
+        WriteLine("da-DK: Danish (Denmark)");
+        WriteLine("fr CA French (Canada)");
+        WriteLine("Enter an ISO culture code");
+        string newCulture = ReadLine();
+
+        if (!string.IsNullOrEmpty(newCulture))
+        {
+            var ci = new CultureInfo(newCulture);
+            CultureInfo.CurrentCulture = ci;
+            CultureInfo.CurrentUICulture = ci;
+        }
+        WriteLine();
+
+        WriteLine("Enter your name: ");
+        string name = ReadLine();
+
+        Write("Enter your DOB: ");
+        string dob = ReadLine();
+
+        Write("Enter your salary: ");
+        string salary = ReadLine();
+
+        DateTime date = DateTime.Parse(dob);
+        int minutes = (int)DateTime.Today.Subtract(date).TotalMinutes;
+        decimal earns = decimal.Parse(salary);
+
+        WriteLine("{0} was born on a {1:dddd}, is {2:N0} minutes old and earns {3:C}", 
+        name, date, minutes, earns);
+        
+
+
+
+    }
+} 
